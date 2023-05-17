a2c83134edd708edf0ec73b5ab7c5dfc135ee835
Cyprian Adamski <cyprian.adamski@gmail.com>
2023-05-17 07:21:50 +0200

    Ran yarn build and modified the server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 const testimonialRoutes = require('./testimonials.routes');
 const concertsRoutes = require('./concerts.routes');
@@ -18,6 +19,15 @@ app.use((req, res, next) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-app.listen(8080, () => {
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+// app.listen(8080, () => {
+//   console.log('Server is running on port: 8080');
+// });
+app.listen(process.env.PORT || 8080, () => {
   console.log('Server is running on port: 8080');
 });
